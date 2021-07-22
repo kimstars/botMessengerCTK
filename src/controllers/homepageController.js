5857291b6338c4896e2cdd0c1e0b86128122d480
chutuankiet <buikika9@gmail.com>
2021-07-22 15:45:33 +0700

    fdsafas

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -156,7 +156,7 @@ let callSendAPI = (sender_psid, response) => {
 
     // Send the HTTP request to the Messenger Platform
     request({
-        "uri": "https://graph.facebook.com/v6.0/me/messages",
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
         "qs": { "access_token": PAGE_ACCESS_TOKEN},
         "method": "POST",
         "json": request_body
